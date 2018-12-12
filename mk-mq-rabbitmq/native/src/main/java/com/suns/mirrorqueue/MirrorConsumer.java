202b9f451cfb62da49ae0266552c10f648bbe38d
mk <sungymf@fansfinancial.com>
2018-12-12 15:09:23 +0800

    HAProxy + RabbitMQ镜像队列

---
+++ mk-mq-rabbitmq/native/src/main/java/com/suns/mirrorqueue/MirrorConsumer.java
@@ -0,0 +1,72 @@
+/**
+ * Project Name:mk-mq <br>
+ * Package Name:com.suns.mirrorqueue <br>
+ *
+ * @author mk <br>
+ * Date:2018-12-12 14:53 <br>
+ */
+
+package com.suns.mirrorqueue;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * ClassName: MirrorConsumer <br>
+ * Description:  <br>
+ * @author mk
+ * @Date 2018-12-12 14:53 <br>
+ * @version
+ */
+public class MirrorConsumer {
+
+    public static void main(String[] argv)
+            throws IOException, TimeoutException {
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("47.107.146.57");
+        connectionFactory.setPort(5672);
+        connectionFactory.setUsername("user001");
+        connectionFactory.setPassword("123456");
+        connectionFactory.setVirtualHost("vhost001");
+
+        // 打开连接和创建频道，与发送端一样
+        Connection connection = connectionFactory.newConnection();
+        final Channel channel = connection.createChannel();
+        channel.exchangeDeclare(MirrorProducer.EXCHANGE_NAME,
+                "fanout");
+
+        /*声明一个队列*/
+        String queueName = "mirror_queue_test";
+        Map<String, Object> arguments = new HashMap<String, Object>();
+        arguments.put("x-ha-policy","all");
+        channel.queueDeclare(queueName,false,false,
+                false,null);
+
+        /*绑定，将队列和交换器通过路由键进行绑定*/
+        String routekey = "error";/*表示只关注error级别的日志消息*/
+        channel.queueBind(queueName,MirrorProducer.EXCHANGE_NAME,routekey);
+
+        System.out.println("waiting for message........");
+
+        /*声明了一个消费者*/
+        final Consumer consumer = new DefaultConsumer(channel){
+            @Override
+            public void handleDelivery(String consumerTag,
+                                       Envelope envelope,
+                                       AMQP.BasicProperties properties,
+                                       byte[] body) throws IOException {
+                String message = new String(body, "UTF-8");
+                System.out.println("Received["+envelope.getRoutingKey()
+                        +"]"+message);
+            }
+        };
+        /*消费者正式开始在指定队列上消费消息*/
+        channel.basicConsume(queueName,true,consumer);
+
+
+    }
+}
---
+++ mk-mq-rabbitmq/native/src/main/java/com/suns/mirrorqueue/MirrorConsumerPolicy.java
@@ -0,0 +1,71 @@
+/**
+ * Project Name:mk-mq <br>
+ * Package Name:com.suns.mirrorqueue <br>
+ *
+ * @author mk <br>
+ * Date:2018-12-12 14:55 <br>
+ */
+
+package com.suns.mirrorqueue;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * ClassName: MirrorConsumerPolicy <br>
+ * Description:  <br>
+ * @author mk
+ * @Date 2018-12-12 14:55 <br>
+ * @version
+ */
+public class MirrorConsumerPolicy {
+
+    public static void main(String[] argv)
+            throws IOException, TimeoutException {
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("47.107.146.57");
+        connectionFactory.setPort(5673);
+        connectionFactory.setUsername("user001");
+        connectionFactory.setPassword("123456");
+        connectionFactory.setVirtualHost("vhost001");
+
+        // 打开连接和创建频道，与发送端一样
+        Connection connection = connectionFactory.newConnection();
+        final Channel channel = connection.createChannel();
+        channel.exchangeDeclare(MirrorProducer.EXCHANGE_NAME,
+                "fanout");
+
+        /*声明一个队列*/
+        String queueName = "mirror_policy_test";
+        Map<String, Object> arguments = new HashMap<String, Object>();
+        channel.queueDeclare(queueName,false,false,
+                false,null);
+
+        /*绑定，将队列和交换器通过路由键进行绑定*/
+        String routekey = "error";/*表示只关注error级别的日志消息*/
+        channel.queueBind(queueName,MirrorProducer.EXCHANGE_NAME,routekey);
+
+        System.out.println("waiting for message........");
+
+        /*声明了一个消费者*/
+        final Consumer consumer = new DefaultConsumer(channel){
+            @Override
+            public void handleDelivery(String consumerTag,
+                                       Envelope envelope,
+                                       AMQP.BasicProperties properties,
+                                       byte[] body) throws IOException {
+                String message = new String(body, "UTF-8");
+                System.out.println("Received["+envelope.getRoutingKey()
+                        +"]"+message);
+            }
+        };
+        /*消费者正式开始在指定队列上消费消息*/
+        channel.basicConsume(queueName,true,consumer);
+
+
+    }
+}
